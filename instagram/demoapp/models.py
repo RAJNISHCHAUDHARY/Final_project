0d60fa25117396a5b4c820efdc2d682afe85f76d
rajneeshchaudhary <rajnishchaudhary786@gmail.com>
2017-07-16 22:29:01 +0530

    files are added

--- instagram/demoapp/models.py
+++ instagram/demoapp/models.py
@@ -6,6 +6,6 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
-    age = models.IntegerField(default=250)
+    mobile = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
     created_on = models.DateTimeField(auto_now_add = True)
