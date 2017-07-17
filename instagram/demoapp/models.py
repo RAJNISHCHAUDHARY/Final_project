0d4a081fa5e84519c72b68c5a0757dcbd89b08a1
rajneeshchaudhary <rajnishchaudhary786@gmail.com>
2017-07-17 11:43:06 +0530

    new files are added

--- instagram/demoapp/models.py
+++ instagram/demoapp/models.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class UserModel(models.Model):
+    Email = models.EmailField()
     name = models.CharField(max_length=255)
-    mobile = models.CharField(max_length=255)
-    age = models.IntegerField(default=0)
+    Username = models.CharField(max_length=128)
+    Password = models.CharField(max_length=40)
     created_on = models.DateTimeField(auto_now_add = True)
+    Updated_on = models.DateTimeField(auto_now=True)
